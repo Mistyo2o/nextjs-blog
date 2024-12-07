83e1158b73556144d623fde0932643f325997306
Misty <8842400+zh_misty@user.noreply.gitee.com>
2024-12-07 18:22:53 +0800

    fix：next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,7 @@ import { build } from "velite";
 export default {
   // othor next config here...
   pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx corretly
-    // Instead, use `not-found.ts` as a workaround
-    // "ts" is required to resolve `not-found.ts`
-    // https://github.com/vercel/next.js/issues/65447
+    "ts",
     "tsx"
 
   ],
